c5720bdf545be2631f6e381a4d8cc01d238f9325
madvas <matus.lestan@gmail.com>
2017-07-16 16:37:21 +0700

    Update dependedcies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject district0x "0.1.14"
+(defproject district0x "0.1.15"
   :description "district0x Frontend Library"
   :url "https://github.com/district0x/d0xINFRA-frontend"
   :license {:name "Eclipse Public License"
@@ -18,7 +18,7 @@
                  [day8.re-frame/http-fx "0.1.3"]
                  [kibu/pushy "0.3.6"]
                  [madvas.re-frame/google-analytics-fx "0.1.0"]
-                 [madvas.re-frame/web3-fx "0.1.11"]
+                 [madvas.re-frame/web3-fx "0.1.12"]
                  [madvas/reagent-patched "0.6.1" :exclusions [cljsjs/react cljsjs/react-dom]]
                  [medley "0.8.3"]
                  [org.clojure/clojurescript "1.9.671"]
